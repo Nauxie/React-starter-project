5a2de6d6ca4d9fdb42aaa3f21054cb381a714733
Nauxie <abhinav.tumu1@gmail.com>
2018-09-12 23:10:32 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var webpack = require("webpack");
+var path = require("path");
+var DIST_DIR = path.resolve (__dirname, "dist");
+var SRC_DIR = path.resolve (__dirname, "src");
+var config = {
+    entry: SRC_DIR + "/app.index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+};
+
+module.exports = config;
